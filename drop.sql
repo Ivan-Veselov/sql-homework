8f9d56772a3182c496b8c85ac2ccd36eeb097302
Ivan Veselov <veselov.iva@gmail.com>
2017-09-24 22:15:58 +0300

    Add script for dropping.

---
+++ drop.sql
@@ -0,0 +1,16 @@
+DROP TABLE vehicles,
+           assignments,
+           volunteers,
+           buildings,
+           delegations,
+           delegation_leaders,
+           athletes,
+           competitions,
+           participations,
+           medals,
+           athletes_specializations,
+           building_specializations,
+           volunteers_assignments;
+           
+DROP TYPE MEDAL_COLOUR, SEX;
+
