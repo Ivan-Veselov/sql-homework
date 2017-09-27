0a9ff14450e54d0246c8a183265def9c02e7c98e
Ivan Veselov <veselov.iva@gmail.com>
2017-09-27 19:08:33 +0300

    Rename position to place.

--- init.sql
+++ init.sql
@@ -71,10 +71,10 @@ CREATE TABLE competitions (
 CREATE TABLE participations (
     competition_id  INTEGER  REFERENCES competitions NOT NULL,
     athlete_id      INTEGER  REFERENCES athletes NOT NULL,
-    position        INTEGER  CHECK (position > 0) NULL,
+    place           INTEGER  CHECK (place > 0) NULL,
     
     UNIQUE (competition_id, athlete_id),
-    UNIQUE (competition_id, position)
+    UNIQUE (competition_id, place)
 );
 
 CREATE TABLE athletes_specializations (
