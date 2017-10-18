fbf3080eda35d2edc80cfc3e588695270ee4543b
Ilya Kaysin <demarkok@gmail.com>
2017-10-18 11:17:31 +0300

    create a skeleton

---
+++ data.sql
@@ -0,0 +1,11 @@
+SET search_path=public; -- I don't know what it means
+
+CREATE OR REPLACE FUNCTION GenerateData() RETURNS VOID AS $$
+BEGIN
+
+
+
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT GenerateData();
