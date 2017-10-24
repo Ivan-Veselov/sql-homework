6a04a2148ae8d163e099a93800dc6d6f7c43db7f
Ivan Veselov <veselov.iva@gmail.com>
2017-10-24 16:28:59 +0300

    Add fourth query.

---
+++ veselov/4.sql
@@ -0,0 +1,7 @@
+SELECT Countries.name, COUNT(1) AS athletes_number
+    FROM Athletes
+    JOIN Delegations  ON Delegations.id = Athletes.delegation_id
+    JOIN Countries    ON Countries.id = Delegations.country_id
+GROUP BY Countries.id
+ORDER BY athletes_number DESC;
+
