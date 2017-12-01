1e4d79d77c6fa9d8134955285c0d9e0968bdb1f4
shavkunov <mv.shavkunov@yandex.ru>
2017-12-01 08:32:02 +0300

    added list-view for list content

---
+++ front/src/components/list-view.js
@@ -0,0 +1,70 @@
+import { Table } from 'semantic-ui-react'
+
+class SpecifiedList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: this.props.data,
+            columns: this.props.columns
+        }
+    }
+
+    renderTableHeader = () => {
+        cells = [];
+        for (var i = 0; i < this.props.columns.length; i++) {
+            let column_name = this.props.columns[i];
+
+            cells.push(
+                <Table.HeaderCell> {column_name} </Table.HeaderCell>
+            );
+        }
+
+        return (
+            <Table.Row>
+                {cells}
+            </Table.Row>
+        );
+    };
+
+    renderTableBody = () => {
+        rows = []
+        for (var i = 0; i < this.props.data.length; i++) {
+            let current_row = this.props.data[i];
+            let row_cells = [];
+            for (var j = 0; j < current_row.length; j++) {
+                let current_row_item = current_row[j]
+
+                row_cells.push(
+                    <Table.Cell> {current_row_item} </Table.Cell>
+                );
+            }
+
+            cells.push(
+                <Table.Row>
+                    {row_cells}
+                </Table.Row>
+            );
+        }
+
+        return (
+            {rows}
+        );
+    };
+
+    render() {
+        return (
+            <Table singleLine compact>
+                <Table.Header>
+                    {this.renderTableHeader()}
+                </Table.Header>
+
+                <Table.Body>
+                    {this.renderTableBody()}
+                </Table.Body>
+            </Table>
+        );
+    }
+}
+
+export default SpecifiedList;
--- front/src/components/ui.js
+++ front/src/components/ui.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react'
+import SpecifiedList from './list-view.js';
 
 /**
     Center align
@@ -10,20 +11,83 @@ const center = {
     flexWrap: "wrap",
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: "20px",
 };
 
 
 class UI extends React.Component {
     constructor() {
         super();
-        this.state = {}
+
+        this.state = {isContentRendered: false};
     }
 
     /**
         On sportmen click -- view all sportsmen
         On accomodation click -- view all accomodations
     */
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+    handleItemClick = (e, { name }) => {
+        this.setState({ activeItem: name });
+
+        switch (name) {
+            case 'all_sportsmen':
+                this.sendAllSportsmenQuery()
+
+            case 'all_accomodations':
+                this.sendAllAccomodationsQuery()
+        }; 
+    };
+
+    // TODO: extract this to another query handle file
+    sendAllSportsmenQuery = () => {
+        let query_name = "/sportsman/all";
+        let params = "";
+
+        data_handler = (response) => {
+            this.state = {
+                isContentRendered: true,
+                table_header: ["Имя", "Фамилия"],
+                table_body: response
+            };
+        };
+    };
+
+    sendAllAccomodationsQuery = () => {
+        let query_name = "/accomodation/all";
+        let params = "";
+
+        data_handler = (response) => {
+            this.state = {
+                isContentRendered: true,
+                table_header: ["Название", "Адрес"],
+                table_body: response
+            };
+        };
+    };
+
+    sendQuery = (query_name, params, data_handler) => {
+        let requestUrl = encodeURI("http://localhost:1234/" + query_name + "/" + params);
+        $.ajax({
+            type: "GET",
+            url: requestUrl,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(response) {
+                data_handler(response);
+            }
+        });
+    };
+
+    renderQueryContent = () => {
+        if (isContentRendered) {
+            return (
+                <SpecifiedList 
+                    data={this.state.table_body}
+                    columns={this.state.table_header}            
+                />
+            );
+        }
+    };
 
     /**
      * Rendering tabs together.
@@ -37,21 +101,21 @@ class UI extends React.Component {
 
                 <Menu>
                     <Menu.Item
-                      name='sportmen'
-                      active={activeItem === 'sportmen'}
+                      name='all_sportmen'
+                      active={activeItem === 'all_sportsmen'}
                       content='Список всех спортсменов'
                       onClick={this.handleItemClick}
                     />
 
                     <Menu.Item
-                      name='accomodations'
-                      active={activeItem === 'accomodations'}
+                      name='all_accomodations'
+                      active={activeItem === 'all_accomodations'}
                       content='Список всех помещений'
                       onClick={this.handleItemClick}
                     />
                 </Menu>
 
-                <h1> TODO click list </h1>
+                {this.renderQueryContent()}
             </div>
         );
     }
