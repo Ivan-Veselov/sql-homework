69f52b6185d86dadfdf5e57bf2c53f76e5f720cb
Ivan Veselov <veselov.iva@gmail.com>
2017-12-07 13:14:49 +0300

    Add interface for Data Base Manager.

--- server/src/main/kotlin/ru/spbau/mit/bachelors2015/DataBaseManager.kt
+++ server/src/main/kotlin/ru/spbau/mit/bachelors2015/DataBaseManager.kt
@@ -1,31 +1,47 @@
 package ru.spbau.mit.bachelors2015
 
-object DataBaseManager {
-    fun allAthletes(accommodationId: Int?) : List<AthleteBrief> {
+interface DataBaseManager {
+    fun allAthletes(accommodationId: Int?) : List<AthleteBrief>
+
+    fun allAccommodations() : List<AccommodationBrief>
+
+    fun allVolunteers() : List<AccommodationBrief>
+
+    fun getAthlete(athleteId: Int) : Athlete
+
+    fun getAccommodation(accommodationId: Int) : Accommodation
+
+    fun getVolunteer(volunteerId: Int) : Volunteer
+
+    fun setAthleteInfo(athleteId: Int, accommodationId: Int, volunteerId: Int)
+}
+
+object DataBaseManagerImpl : DataBaseManager {
+    override fun allAthletes(accommodationId: Int?) : List<AthleteBrief> {
         TODO("not implemented")
     }
 
-    fun allAccommodations() : List<AccommodationBrief> {
+    override fun allAccommodations() : List<AccommodationBrief> {
         TODO("not implemented")
     }
 
-    fun allVolunteers() : List<AccommodationBrief> {
+    override fun allVolunteers() : List<AccommodationBrief> {
         TODO("not implemented")
     }
 
-    fun getAthlete(athleteId: Int) : Athlete {
+    override fun getAthlete(athleteId: Int) : Athlete {
         TODO("not implemented")
     }
 
-    fun getAccommodation(accommodationId: Int) : Accommodation {
+    override fun getAccommodation(accommodationId: Int) : Accommodation {
         TODO("not implemented")
     }
 
-    fun getVolunteer(volunteerId: Int) : Volunteer {
+    override fun getVolunteer(volunteerId: Int) : Volunteer {
         TODO("not implemented")
     }
 
-    fun setAthleteInfo(athleteId: Int, accommodationId: Int, volunteerId: Int) {
+    override fun setAthleteInfo(athleteId: Int, accommodationId: Int, volunteerId: Int) {
         TODO("not implemented")
     }
 }
