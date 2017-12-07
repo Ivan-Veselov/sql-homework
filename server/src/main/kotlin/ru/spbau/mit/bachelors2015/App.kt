396c495987daa3654fbc6d7e0a596e4e69cc257d
Ivan Veselov <veselov.iva@gmail.com>
2017-12-07 13:23:15 +0300

    Add classes skeletons.

--- server/src/main/kotlin/ru/spbau/mit/bachelors2015/App.kt
+++ server/src/main/kotlin/ru/spbau/mit/bachelors2015/App.kt
@@ -1,5 +1,44 @@
 package ru.spbau.mit.bachelors2015
 
+class Server(database: DataBaseManager) {
+    // TODO
+}
+
+class DullDataBaseManager : DataBaseManager {
+    private val athletes: List<Athlete> = TODO("not implemented")
+
+    private val volunteers: List<Volunteer> = TODO("not implemented")
+
+    private val accommodations: List<Accommodation> = TODO("not implemented")
+
+    override fun allAthletes(accommodationId: Int?): List<AthleteBrief> {
+        TODO("not implemented")
+    }
+
+    override fun allAccommodations(): List<AccommodationBrief> {
+        TODO("not implemented")
+    }
+
+    override fun allVolunteers(): List<AccommodationBrief> {
+        TODO("not implemented")
+    }
+
+    override fun getAthlete(athleteId: Int): Athlete {
+        TODO("not implemented")
+    }
+
+    override fun getAccommodation(accommodationId: Int): Accommodation {
+        TODO("not implemented")
+    }
+
+    override fun getVolunteer(volunteerId: Int): Volunteer {
+        TODO("not implemented")
+    }
+
+    override fun setAthleteInfo(athleteId: Int, accommodationId: Int, volunteerId: Int) {
+    }
+}
+
 fun main(args: ArrayList<String>) {
     TODO("not implemented")
 }
