cd1138012b01ea0f9f5b28e7ac3f513402c69c37
Ivan Veselov <veselov.iva@gmail.com>
2017-12-07 18:01:32 +0300

    Add artificial data.

--- server/src/main/kotlin/ru/spbau/mit/bachelors2015/App.kt
+++ server/src/main/kotlin/ru/spbau/mit/bachelors2015/App.kt
@@ -5,11 +5,67 @@ class Server(database: DataBaseManager) {
 }
 
 class DullDataBaseManager : DataBaseManager {
-    private val athletes: List<Athlete> = TODO("not implemented")
+    private val volunteers: List<Volunteer> =
+        listOf(
+            Volunteer(
+                "Hagar Broz",
+                "+7 999 2286942"
+            ),
+            Volunteer(
+                "Zviad Levitt",
+                "+7 666 6669666"
+            ),
+            Volunteer(
+                "Breeshey Colón",
+                "+7 000 9110000"
+            )
+        )
 
-    private val volunteers: List<Volunteer> = TODO("not implemented")
+    private val accommodations: List<Accommodation> =
+        listOf(
+            Accommodation("Academy Street", 42, "hotel", "Sun Heaven"),
+            Accommodation("Jefferson Avenue", 228, "hostel", null),
+            Accommodation(
+                "Church Road",
+                69,
+                "hutments",
+                "A place for my head"
+            )
+        )
 
-    private val accommodations: List<Accommodation> = TODO("not implemented")
+    private val athletes: List<Athlete> =
+        listOf(
+            Athlete(
+                "LeBron James",
+                "male",
+                203,
+                113,
+                32,
+                accommodations[0].brief(1),
+                "USA",
+                volunteers[0].brief(1)
+            ),
+            Athlete(
+                "Dwyane Wade",
+                "male",
+                193,
+                100,
+                35,
+                null,
+                "USA",
+                null
+            ),
+            Athlete(
+                "Serena Williams",
+                "female",
+                175,
+                90,
+                36,
+                null,
+                "USA",
+                volunteers[1].brief(2)
+            )
+        )
 
     override fun allAthletes(accommodationId: Int?): List<AthleteBrief> {
         TODO("not implemented")
