3cc1ec3818911b2bedde17ab8d0f81a095703c89
Ilya Kaysin <demarkok@gmail.com>
2017-12-14 13:17:57 +0300

    add port parameter to deployment script

--- startDatabase.sh
+++ startDatabase.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+port=5432
+
+if [ "$1" != "" ]; then
+    port=$1
+fi
+
 sudo docker build -t olympic_db .
-sudo docker run -p 5432:5432 -it olympic_db
+sudo docker run -p $port:5432 -it olympic_db
 
